45270d51e1707600955a234dfae30877a53ec1ef
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-04-18 11:43:52 +0900

    서바이벌 구현

---
+++ Source/DFProject/Public/Server/DFAwardsGameMode.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DFAwardsGameMode.generated.h"
+
+
+UCLASS()
+class DFPROJECT_API ADFAwardsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+	/** Tag 이름으로 TargetPoint 찾아주는 헬퍼 */
+	AActor* FindRankPoint(const FName& Tag) const;
+};
